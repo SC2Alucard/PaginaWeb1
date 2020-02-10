af9bd5d6e4af6137c07b92d945b73980f7f393aa
SC2Alucard <alcuard.moyajimenez076@hotmail.com>
2020-02-10 01:20:21 -0400

    Updates

---
+++ precache-manifest.860691b853b7f11f8f54bc53c47c6f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1618ba28643796344d78fc64d348ad37",
+    "url": "/PaginaWeb1/index.html"
+  },
+  {
+    "revision": "e47625f2cd63388a6b88",
+    "url": "/PaginaWeb1/static/css/main.4a57bec6.chunk.css"
+  },
+  {
+    "revision": "c29f0e4b0601e1999046",
+    "url": "/PaginaWeb1/static/js/2.1c649832.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PaginaWeb1/static/js/2.1c649832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47625f2cd63388a6b88",
+    "url": "/PaginaWeb1/static/js/main.1648fdfa.chunk.js"
+  },
+  {
+    "revision": "17a2079c3232a8b7254b",
+    "url": "/PaginaWeb1/static/js/runtime-main.11ce3057.js"
+  }
+]);
